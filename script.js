69808ad5922d8aea65ddc6decbf723257a1b8cf5
pawansevak <pssevak@gmail.com>
2016-06-21 16:51:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`I didn't understand that. I am a very young bot. Could you type please type START to know more about my master, Pawan.`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
